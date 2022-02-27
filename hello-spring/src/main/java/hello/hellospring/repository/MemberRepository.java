a30a5373f74dfee1ce9de45835761d3db6f96f77
nathan29849 <phs960418@gmail.com>
2022-02-27 23:54:39 +0900

    add : 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);  // Optional을 통해 null로 반환되는 경우를 대비함
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
